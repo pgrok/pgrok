4c11f1b4c77571f2be8d1dd1a516d6cbbc0b1481
Joe Chen <jc@unknwon.io>
2023-08-22 11:09:53 -0400

    Dockerfile: fix webbuilder copy dest path

---
+++ pgrokd/cli/ssh_server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/charmbracelet/log"
+
+	"github.com/pgrok/pgrok/internal/conf"
+	"github.com/pgrok/pgrok/internal/database"
+	"github.com/pgrok/pgrok/internal/reverseproxy"
+	"github.com/pgrok/pgrok/internal/sshd"
+)
+
+func startSSHServer(logger *log.Logger, sshdPort int, proxy conf.Proxy, db *database.DB, proxies *reverseproxy.Cluster) {
+	logger = logger.WithPrefix("sshd")
+	err := sshd.Start(
+		logger,
+		sshdPort,
+		proxy,
+		db,
+		func(host, forward string) { proxies.Set(host, forward) },
+		func(host string) { proxies.Remove(host) },
+	)
+	if err != nil {
+		logger.Fatal("Failed to start server", "error", err)
+	}
+}
