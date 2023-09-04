eb34ef269158aaa3dedf528df268c4370cc0ee25
Joe Chen <jc@unknwon.io>
2023-09-03 23:17:25 -0400

    feat: support user sign out

--- pgrokd/web/src/pages/Dashboard.tsx
+++ pgrokd/web/src/pages/Dashboard.tsx
@@ -1,5 +1,7 @@
-import { Disclosure } from "@headlessui/react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import useUser from "../hooks/useUser";
 
@@ -36,6 +38,36 @@ export default function DashboardPage() {
                         </Link>
                       ))}
                     </div>
+                  </div>
+
+                  <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                    {/* Profile dropdown */}
+                    <Menu as="div" className="relative ml-3">
+                      <div>
+                        <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                          <span className="absolute -inset-1.5" />
+                          <span className="sr-only">Open user menu</span>
+                          <UserCircleIcon className="h-8 w-8 rounded-full" />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item>
+                            <a href="/-/sign-out" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                              Sign out
+                            </a>
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
                   </div>
                 </div>
               </div>
